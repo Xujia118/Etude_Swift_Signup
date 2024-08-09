b7663d8a7ddcfd9ec9470fa1003888498481f449
Jia Xu <xujia118@hotmail.com>
2024-08-09 15:09:33 -0700

    Initial Commit

---
+++ Etude_SignUp/Etude_SignUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Etude_SignUpApp.swift
+//  Etude_SignUp
+//
+//  Created by xujia on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Etude_SignUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
